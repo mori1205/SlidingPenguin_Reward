80c79cfb2fb8e7a4373fa6beda22b9441b395b9a
mori1205 <mm2384mm@gm.tsuda.ac.jp>
2024-12-01 11:25:18 +0900

    スタート画面変更

--- Assets/Scripts/InGame/InGameStatusManager.cs
+++ Assets/Scripts/InGame/InGameStatusManager.cs
@@ -7,33 +7,33 @@ using UnityEngine.UI;
 
 namespace penguin
 {
-    public enum InGameStatus
+  public enum InGameStatus
+  {
+    Null,
+    StageIntroduction,
+    CountDown,
+    InGameNormal,
+    HurryUp,
+    ReachGoal,
+    CourseOut,
+    TimeUp
+  }
+
+  public class InGameStatusManager : MonoBehaviour
+  {
+    public InGameStatus CurrentStatus;
+
+    // Start is called before the first frame update
+    private void Start()
     {
-        Null,
-        StageIntroduction,
-        CountDown,
-        InGameNormal,
-        HurryUp,
-        ReachGoal,
-        CourseOut,
-        TimeUp
+      StartCoroutine(Initialization());
     }
 
-    public class InGameStatusManager : MonoBehaviour
+    private IEnumerator Initialization()
     {
-        public InGameStatus CurrentStatus;
-        
-        // Start is called before the first frame update
-        private void Start()
-        {
-            StartCoroutine(Initialization());
-        }
-    
-        private IEnumerator Initialization()
-        {
-            if (ParameterManager.gameAnimation) { yield return new WaitForSeconds(1.5f); }
-            else { yield return new WaitForSeconds(0.0f); }
-            CurrentStatus = InGameStatus.StageIntroduction;
-        }   
+      if (ParameterManager.gameAnimation) { yield return new WaitForSeconds(1.5f); }
+      else { yield return new WaitForSeconds(0.0f); }
+      CurrentStatus = InGameStatus.StageIntroduction;
     }
+  }
 }
--- Assets/Scripts/Parameter/ParameterManager.cs
+++ Assets/Scripts/Parameter/ParameterManager.cs
@@ -2,20 +2,20 @@ using UnityEngine;
 
 public class ParameterManager : MonoBehaviour
 {
-    // game parameters
-    public static float sensitivity = 3.0f;
-    public static int limitedTime = 120;
-    public static float maximumSpeed = 15.0f;
-    public static float acceleration = 0.050f;
-    public static float friction = 0.9990f;
+  // game parameters
+  public static float sensitivity = 3.0f;
+  public static int limitedTime = 120;
+  public static float maximumSpeed = 15.0f;
+  public static float acceleration = 0.050f;
+  public static float friction = 0.9990f;
 
-    // experiment parameters
-    public static bool fish = true;
-    public static bool gameAnimation = true;
-    public static bool respawn = true;
-    public static bool continuousPlay = false;
-    public static float waitTimeNext = 5.0f;
+  // experiment parameters
+  public static bool fish = true;
+  public static bool gameAnimation = true;
+  public static bool respawn = true;
+  public static bool continuousPlay = false;
+  public static float waitTimeNext = 5.0f;
 
-    // for development
-    public static bool usePhysics = true;
+  // for development
+  public static bool usePhysics = true;
 }
