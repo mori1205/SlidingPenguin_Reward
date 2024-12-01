42ac020e395869bb5edaa9d11efb97509930976c
mori1205 <mm2384mm@gm.tsuda.ac.jp>
2024-12-01 17:20:56 +0900

    物理ボタン追加，制限時間変更

--- Assets/Scripts/Parameter/ParameterManager.cs
+++ Assets/Scripts/Parameter/ParameterManager.cs
@@ -4,7 +4,7 @@ public class ParameterManager : MonoBehaviour
 {
   // game parameters
   public static float sensitivity = 3.0f;
-  public static int limitedTime = 120;
+  public static int limitedTime = 180;
   public static float maximumSpeed = 15.0f;
   public static float acceleration = 0.050f;
   public static float friction = 0.9990f;
--- Assets/Scripts/Start/HomeUISwitcher.cs
+++ Assets/Scripts/Start/HomeUISwitcher.cs
@@ -8,30 +8,44 @@ using UnityEngine.Serialization;
 
 namespace penguin
 {
-    public class HomeUISwitcher : MonoBehaviour
+  public class HomeUISwitcher : MonoBehaviour
+  {
+    // 表示をスイッチするキャンバス
+    [SerializeField] private GameObject homeCanvas;
+    [SerializeField] private GameObject adjustCanvas;
+
+    bool buttonOn = false;
+
+    public void ActivateHomeUI()
     {
-        // 表示をスイッチするキャンバス
-        [SerializeField] private  GameObject homeCanvas;
-        [SerializeField] private  GameObject adjustCanvas;
-      
-        public void ActivateHomeUI()
-        {
-            homeCanvas.SetActive(true);
-            adjustCanvas.SetActive(false);
-        }
-        
-        public void ActivateSettingUI()
-        {
-            adjustCanvas.SetActive(true);
-            homeCanvas.SetActive(false);
-        }
-        
-        public IEnumerator ActivateInGameUI()
-        {
-            yield return new WaitForSeconds(0.8f);
-            SceneManager.LoadScene ("InGame");
-        }
-        
+      homeCanvas.SetActive(true);
+      adjustCanvas.SetActive(false);
     }
+
+    public void ActivateSettingUI()
+    {
+      adjustCanvas.SetActive(true);
+      homeCanvas.SetActive(false);
+    }
+
+    public IEnumerator ActivateInGameUI()
+    {
+      yield return new WaitForSeconds(0.8f);
+      SceneManager.LoadScene("InGame");
+
+
+      if (buttonOn == true || Input.GetKey(KeyCode.Q))
+      {
+        yield return new WaitForSeconds(0.8f);
+        SceneManager.LoadScene("InGame");
+      }
+    }
+
+    public void ButtonOn()
+    {
+      buttonOn = true;
+    }
+
+  }
 
 }
